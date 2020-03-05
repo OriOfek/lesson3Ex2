b309e4b1a042909f290a5226b291c3d93b77c570
OriOfek <aa6055@bs.amalnet.k12.il>
2020-03-05 17:52:10 +0200

    finish

--- app/src/main/java/com/example/lesson3ex2/MainActivity.java
+++ app/src/main/java/com/example/lesson3ex2/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     TextView accurate;
     TextView number1;
     TextView number2;
-
+    boolean start;
 
 
 
@@ -52,83 +52,82 @@ public class MainActivity extends AppCompatActivity {
         number1=(TextView) findViewById(R.id.firstnumber);
         number2=(TextView) findViewById(R.id.secondnumber);
         accurate=(TextView) findViewById(R.id.trueFalse);
-
+        start=false;
 
     }
 
     public void setnumber(View view) {
+        start=true;
         click=true;
-        num1=rnd.nextInt(20);
-        num2=rnd.nextInt(20);
+        num1=rnd.nextInt(101);
+        num2=rnd.nextInt(101);
         number1.setText(String.valueOf(num1));
         number2.setText(String.valueOf(num2));
     }
 
     public void big(View view) {
-        if (click==true) {
-            countervalue++;
+        if (start != false) {
+            if (click == true) {
+                countervalue++;
+            }
+            if ((click == true) && (num2 > num1)) {
+                counteraccurate++;
+            }
+            counter.setText(String.valueOf(counteraccurate / countervalue));
+            if (num2 > num1) {
+                accurate.setText("true");
+            } else {
+                accurate.setText("false");
+            }
+            click = false;
         }
-        if ((click==true) && (num2>num1))
-        {
-            counteraccurate++;
-        }
-        counter.setText(String.valueOf(counteraccurate/countervalue));
-        if (num2>num1)
-        {
-            accurate.setText("true");
-        }
-        else {
-            accurate.setText("false");
-        }
-        click=false;
-        }
+    }
 
     public void worth(View view) {
-        if (click==true) {
-            countervalue++;
-        }
-        if ((click==true) && (num2==num1))
-        {
-            counteraccurate++;
-        }
-        counter.setText(String.valueOf(counteraccurate/countervalue));
-        if (num2==num1)
-        {
-            accurate.setText("true");
-        }
-        else {
-            accurate.setText("false");
+        if (start != false) {
+            if (click == true) {
+                countervalue++;
+            }
+            if ((click == true) && (num2 == num1)) {
+                counteraccurate++;
+            }
+            counter.setText(String.valueOf(counteraccurate / countervalue));
+            if (num2 == num1) {
+                accurate.setText("true");
+            } else {
+                accurate.setText("false");
 
+            }
+            click = false;
         }
-        click=false;
     }
 
     public void small(View view) {
-        if (click==true) {
-            countervalue++;
-        }
-        if ((click==true) && (num2<num1))
-        {
-            counteraccurate++;
-        }
-        counter.setText(String.valueOf(counteraccurate/countervalue));
-        if (num2<num1)
-        {
-            accurate.setText("true");
-        }
-        else {
-            accurate.setText("false");
+        if (start != false) {
+            if (click == true) {
+                countervalue++;
+            }
+            if ((click == true) && (num2 < num1)) {
+                counteraccurate++;
+            }
+            counter.setText(String.valueOf(counteraccurate / countervalue));
+            if (num2 < num1) {
+                accurate.setText("true");
+            } else {
+                accurate.setText("false");
 
+            }
+            click = false;
         }
-        click=false;
     }
 
     public void clear(View view) {
         counteraccurate=0;
+        start=false;
         countervalue=0;
-        counter.setText(String.valueOf(counteraccurate/countervalue));
+        counter.setText("counter");
         accurate.setText("none");
-        number1.setText("number not found");
-        number2.setText("number not found");
+        number1.setText("not found");
+        number2.setText("not found");
     }
 }
