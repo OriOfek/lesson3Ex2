44741e56878382b22b031953ed3f817e300e8809
OriOfek <aa6055@bs.amalnet.k12.il>
2020-03-05 16:45:53 +0200

    work xml

--- app/src/main/java/com/example/lesson3ex2/MainActivity.java
+++ app/src/main/java/com/example/lesson3ex2/MainActivity.java
@@ -3,8 +3,23 @@ package com.example.lesson3ex2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    Random rnd;
+    int num1;
+    int num2;
+    Button clear;
+    Button worth;
+    Button bigger;
+    Button smaller;
+    Button press;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
